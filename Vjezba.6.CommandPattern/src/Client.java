2488aa02781e216132abb5f343023dc76aac734b
venci-ivos <35291480+venci-ivos@users.noreply.github.com>
2018-12-03 19:44:22 +0100

    Add files via upload

--- Vjezba.6.CommandPattern/src/Client.java
+++ Vjezba.6.CommandPattern/src/Client.java
@@ -2,35 +2,42 @@
 public class Client {
 
 	public static void main(String[] args) {
-
+		
+		//Invoker
 		RemoteControl remote = new RemoteControl();
-
+		
+		//Recivers
 		Light light = new Light();
 		GarageDoor garageDoor = new GarageDoor();
 		Stereo stereo = new Stereo();
-
+		
+		//Command objects On--> for On buttons
 		LightOnCommand lghtOnCmd = new LightOnCommand(light);
-		LightOffCommand lghtOffCmd = new LightOffCommand(light);
-
 		GarageOpenCommand garageOpenCmd = new GarageOpenCommand(garageDoor);
-		GarageCloseCommand garageCloseCmd = new GarageCloseCommand(garageDoor);
-
 		StereoOnCommand stereoOnCmd = new StereoOnCommand(stereo);
+
+		//Command objects Off--> for Off buttons
+		LightOffCommand lghtOffCmd = new LightOffCommand(light);
+		GarageCloseCommand garageCloseCmd = new GarageCloseCommand(garageDoor);
 		StereoOffCommand stereoOffCmd = new StereoOffCommand(stereo);
 
+		//Programing buttons
 		remote.setCommand(0, lghtOnCmd, lghtOffCmd);
 		remote.setCommand(1, garageOpenCmd, garageCloseCmd);
 		remote.setCommand(2, stereoOnCmd, stereoOffCmd);
-
+		
+		//Listing programed buttons
 		System.out.println(remote.toString());
-
+		
+		//Button pressed simulation
 		for(int i=0;i<7;i++) {
 			
 			System.out.println("++++ SLOT " +  i + " +++++");
 			remote.buttonOnPressed(i);
 			remote.buttonOffPressed(i);
 		}
-
+		
+		//Testing undo button
 		System.out.println("++++ testing undo +++++");
 
 		remote.buttonOnPressed(1);
